a6d7bf2d9f519ecfd742d42847e072a60fd2152a
Atendra Kumar Kushwaha <atendrakushwaha9685@gmail.com>
2025-02-05 16:06:16 +0530

    data type and variables

---
+++ DataType.js
@@ -0,0 +1,10 @@
+"use strict"; //treat all js code as newer version
+// alert("Hello World");
+let name = "Atendra"; //string
+let age = 18; //number => integer 2  to power 53
+let isLoggedIn = false; //boolean => true or false 
+let state; //undefined
+let country = null; //null
+//Symbol //newly introduced in ES6 used to create unique identifier
+// typeof operator checks the type of variable
+console.log(typeof null);// null is an object
--- Test.js
+++ Test.js
@@ -1,1 +1,15 @@
 console.log("Hello World");
+const accountId = 12234;
+let accountEmail = "V5Yd4@example.com";
+var accountPassword = "12345";
+accountCity = "Mumbai";
+let accountState;
+/* 
+Prefer not to use var
+because of issue in block scope and functional scope.
+*/
+
+accountEmail = "ak@123.com";
+accountPassword = "123456";
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
